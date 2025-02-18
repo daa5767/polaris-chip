59499785b0ae481e20315bd3dbca3e2f014b5b4e
daa5767 <daa5767@psu.edu>
2025-02-18 12:43:52 -0500

    added card

--- src/my-card.js
+++ src/my-card.js
@@ -1,10 +1,11 @@
-import { LitElement, html, css } from 'lit';
 
 /**
  * Now it's your turn. Here's what we need to try and do:
  * 1. Get you HTML from your card working in here 
  * 2. Get your CSS rescoped as needed to work here
  */
+
+import { LitElement, html, css } from 'lit';
 
 export class MyCard extends LitElement {
 
@@ -14,24 +15,95 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "The Big Apple";
+    this.image = "https://www.aroundtheworldl.com/wp-content/uploads/2015/04/DSC02105-1.jpg";
+    this.content = "This is an image of New York City";
+    this.fancy = false;
+
+    
   }
 
   static get styles() {
     return css`
       :host {
         display: block;
+        background-color: white;
+        color: teal;
+        font-size: 24px;
+        padding: 24px;
+        margin: 8px;
+        width:400px;
+        text-align:center;
+        display: block;
+        display: inline-block;
       }
-    `;
+      :host([fancy]) {
+      display: block;
+      background-color: pink;
+      .card-header {
+        font-size: 24px;
+        font-weight: bold;
+        color: #1ac1e6;
+      }
+      div{
+        width:20px;
+
+      }
+      .card-image{
+        width:300px;
+        height:300px;
+      }
+      details summary {
+    text-align: left;
+    font-size: 20px;
+    padding: 8px 0;
   }
 
+  details[open] summary {
+    font-weight: bold;
+  }
+  
+  details div {
+    border: 2px solid black;
+    text-align: left;
+    padding: 8px;
+    height: 70px;
+    overflow: auto;
+  }
+
+    `;
+    
+  }
+  
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+    
+  }
+  
+
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div>
+      <div class ="card-header">${this.title}</div>
+
+      <img class="card-image" src="${this.image}" alt="${this.title}" />
+        <div class="card-description">${this.content}</div>
+      </div>
+      `;   
   }
 
   static get properties() {
     return {
-      title: { type: String },
+      title: { type: String, reflect: true },
+      image: {type: String, reflect: true},
+      content: {type: String, reflect: true},
+      fancy: { type: Boolean, reflect: true }
     };
   }
 }
